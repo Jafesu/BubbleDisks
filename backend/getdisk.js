fa1e45dd878d0dfb3782d4b09b95acbe9b14022d
Ezekiel Hammond <jafesu@outlook.com>
2018-11-15 20:45:43 -0600

    fixed getDisks.js

--- backend/getdisk.js
+++ backend/getdisk.js
@@ -2,27 +2,11 @@ const njds = require('nodejs-disks');
 const { MongoClient } = require('mongodb');
 
 
-(async function mongo() {
-    let client;
-    const url = 'mongodb://localhost:27017';
-    const dbName = 'diskInfo';
-    try {
-        client = await MongoClient.connect(url);
-        console.log('Connected to server');
 
-        const db = client.db(dbName);
-        const disks = await getDisks();
-        const response = await db.collection('disks').insertMany(disks);
-        console.log(response)
-    } catch (err) {
-        console.log(err.stack);
-    }
-
-    client.close();
-}());
 
 const getDisks = () => {
-    
+    console.log("Would not have been possible without my trusty RubberDucky Fuster");
+    let disks = [];
     njds.drives(
       function (err, drives) {
         njds.drivesDetail(
@@ -30,14 +14,32 @@ const getDisks = () => {
           function (err, data) {
             for (var i = 0; i < data.length; i++) {
               if (data[i].mountpoint != null) {
-                disk = `{mount: ${data[i].mountpoint}, total: ${parseFloat(data[i].total.replace(' GB', ''))},free: ${parseFloat(data[i].available.replace(' GB', ''))}, used: ${parseFloat(data[i].used.replace(' GB', ''))}, name: ${data[i].drive}, usedPer: ${data[i].usedPer}, freePer: ${data[i].freePer}}`;
-                disks.push(disk);
+                disks.push({'mount': data[i].mountpoint, 'total': parseFloat(data[i].total.replace(' GB', '')), 'free': parseFloat(data[i].available.replace(' GB', '')), 'used': parseFloat(data[i].used.replace(' GB', '')), 'name': data[i].drive, 'usedPer': data[i].usedPer, 'freePer': data[i].freePer});
               }
             }
-            return disks;
+            console.log(disks);
+            (async function mongo() {
+                let client;
+                const url = 'mongodb://localhost:27017';
+                const dbName = 'diskInfo';
+                try {
+                    client = await MongoClient.connect(url);
+                    console.log('Connected to server');
+            
+                    const db = client.db(dbName);
+                    const response = await db.collection('disks').insertMany(disks);
+                    console.log(response)
+                } catch (err) {
+                    console.log(err.stack);
+                }
+            
+                client.close();
+            }());
             
           }
         )
       }
     ) 
   }
+
+  getDisks();
